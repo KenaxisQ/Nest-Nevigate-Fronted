c807e4daebcc811a47813290f81f5dee30e0dd17
Bhavaniadapa <bhavaniadapa07@gmail.com>
2024-11-16 18:43:37 +0530

    gmail authenticate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import Landing from './Components/Landing/Landing';
-
+import { useState } from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import { LoginPage } from './Components/Login/LoginPage';
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
     <div className="App">
-      <Landing/>
+      <GoogleOAuthProvider clientId="ClientId">
+        <GoogleLogin
+            onSuccess={credentialResponse => {
+              setIsAuthenticated(true);
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log('Login Failed');
+            }}
+          />
+          {!isAuthenticated && <LoginPage />}
+            {isAuthenticated && <Landing/>}
+      </GoogleOAuthProvider>
     </div>
-  );
+  )
 }
 
 export default App;
