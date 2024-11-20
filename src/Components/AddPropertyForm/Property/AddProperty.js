7eb65fbd77fcf2c4f60e9b69d6fea18d80c960dc
TAMMIREDDYSIDDU0 <18BCS4548@gmail.com>
2024-11-20 13:56:53 +0530

    PropertyForm Spacing Adjustment

--- src/Components/AddPropertyForm/Property/AddProperty.js
+++ src/Components/AddPropertyForm/Property/AddProperty.js
@@ -4,7 +4,7 @@ import { FirstForm } from "../PropertyForms/FirstForm"
 export const AddProperty = () => {
     return(
         <>
-            <h1>Add Property</h1>
+            <h1 style={{marginTop:'80px'}}>Add Property</h1>
             <p>Fill the form below to list a property , Remember each listing requires 50 tokens.</p>
             <ProgressBar />
         </>
--- src/Components/AddPropertyForm/PropertyForm.js
+++ src/Components/AddPropertyForm/PropertyForm.js
@@ -11,7 +11,7 @@ export const PropertyForm = () => {
         <>
             <Navbar />
             <div className="property-main">
-                <UserInfo />
+                {/* <UserInfo /> */}
             </div>
             <AddProperty />
         </>
